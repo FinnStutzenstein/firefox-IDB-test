30250e9a450cc77291b0170e6341e8c14b261d85
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2019-04-05 08:24:05 +0200

    Added test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { LocalStorage } from '@ngx-pwa/local-storage';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firefox-IDB-test';
+
+  public constructor(protected localStorage: LocalStorage) {
+    console.log(localStorage);
+    setTimeout(() => {
+      this.doIt();
+    }, 100)
+  }
+
+  public async doIt(): Promise<void> {
+    console.log("Read");
+    let val = await this.localStorage.getItem<string>('myKey').toPromise();
+    console.log('Read: ', val);
+    if (!val) {
+      val = 'Test string';
+    }
+
+    try {
+      console.log("write");
+      await this.localStorage.setItem('myKey', val).toPromise();
+      console.log('set item successfully');
+    } catch (e) {
+      console.log('Error setting item: ', e);
+    }
+  }
 }
